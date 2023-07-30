5426cb8cfad2e9978ebb0eb62f6ac3b541d6a009
Snthilkumar <139695434+Snthilkumar@users.noreply.github.com>
2023-07-30 13:59:41 +0530

    Add files via upload

---
+++ Miss_arr.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Miss_arr
+{
+public static void main(String arg[])
+{
+Scanner o=new Scanner(System.in);
+System.out.println("Enter value");
+int n=o.nextInt();
+int a[]={1,2,3,4};
+int f=a[0];
+int l=a.length-1;
+System.out.println("first"+f);
+System.out.println("Last"+l);
+}}
